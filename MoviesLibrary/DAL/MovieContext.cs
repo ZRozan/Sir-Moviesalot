55b6e08288647838797797321df4c7f21f3320ec
Jairo Rozan <jairo.rosa.z@hotmail.com>
2022-07-21 18:55:27 -0300

    Added DAL initializers

---
+++ MoviesLibrary/DAL/MovieContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+using MoviesLibrary.Models;
+
+
+namespace MoviesLibrary.DAL
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext() : base("MovieContext")
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Director> Directors { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ MoviesLibrary/DAL/MovieInitializer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using MoviesLibrary.Models;
+
+namespace MoviesLibrary.DAL
+{
+    public class MovieInitializer : DropCreateDatabaseIfModelChanges<MovieContext>
+    {
+        protected override void Seed(MovieContext context)
+        {
+            var movies = new List<Movie>
+            {
+                new Movie
+                {
+                    MovieTitle="Equilibrium", ReleaseYear=2002, Duration=107, Imdbscore=7.4f, Metascore=33,
+                    Description="In an oppressive future where all forms of feeling are illegal, a man in charge of enforcing the law rises to overthrow the system and state.",
+                    Poster="https://m.media-amazon.com/images/M/MV5BMTkzMzA1OTI3N15BMl5BanBnXkFtZTYwMzUyMDg5._V1_UY209_CR0,0,140,209_AL_.jpg"
+                }
+            };
+            movies.ForEach(movie => context.Movies.Add(movie));
+            context.SaveChanges();
+
+            var directors = new List<Director>
+            {
+                new Director { Name = "Kurt Wimmer", MovieId = 1 },
+                new Director { Name = "Johnzinho" },
+                new Director { Name = "Zekinha" }
+
+            };
+            directors.ForEach(director => context.Directors.Add(director));
+            context.SaveChanges();
+
+            //Action, Drama, Sci-Fi, Thriller
+
+            var genres = new List<Genre>
+            {
+                new Genre {MovieId = 1, GenreName=GenreName.Action},
+                new Genre {MovieId = 1, GenreName=GenreName.Drama},
+                new Genre {MovieId = 1, GenreName=GenreName.Sci_Fi},
+                new Genre {MovieId = 1, GenreName=GenreName.Thriller}
+            };
+            genres.ForEach(genre => context.Genres.Add(genre));
+            context.SaveChanges();
+        }
+
+    }
+}
