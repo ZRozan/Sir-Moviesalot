21209258c854421f4b9b0f928598852a6a18baa2
Jairo Rozan <jairo.rosa.z@hotmail.com>
2022-07-23 20:16:03 -0300

    updated initializer

--- MoviesLibrary/DAL/MovieInitializer.cs
+++ MoviesLibrary/DAL/MovieInitializer.cs
@@ -15,19 +15,19 @@ namespace MoviesLibrary.DAL
             {
                 new Movie
                 {
-                    MovieTitle="Equilibrium",ReleaseYear=2002,Duration=107,Imdbscore=74,Metascore=33,
+                    Id=1, MovieTitle="Equilibrium",ReleaseYear=2002,Duration=107,Imdbscore=74,Metascore=33,
                     Description="In an oppressive future where all forms of feeling are illegal, a man in charge of enforcing the law rises to overthrow the system and state.",
                     Poster="https://m.media-amazon.com/images/M/MV5BMTkzMzA1OTI3N15BMl5BanBnXkFtZTYwMzUyMDg5._V1_UY209_CR0,0,140,209_AL_.jpg"
                 },
                 new Movie
                 {
-                    MovieTitle="Inception",ReleaseYear=2014,Duration=169,Imdbscore=87,Metascore=74,
+                    Id=2, MovieTitle="Inception",ReleaseYear=2014,Duration=169,Imdbscore=87,Metascore=74,
                     Description="A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster.",
                     Poster="https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_UY209_CR0,0,140,209_AL_.jpg"
                 },
                 new Movie
                 {
-                    MovieTitle="Interstellar",ReleaseYear=2010,Duration=148,Imdbscore=88,Metascore=74,
+                    Id=3, MovieTitle="Interstellar",ReleaseYear=2010,Duration=148,Imdbscore=88,Metascore=74,
                     Description="A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival",
                     Poster="https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UY209_CR0,0,140,209_AL_.jpg"
                 }
@@ -37,32 +37,40 @@ namespace MoviesLibrary.DAL
 
             var directors = new List<Director>
             {
-                new Director { Name = "Kurt Wimmer", MovieId = 1 },
-                new Director { Name = "Christopher Nolan", MovieId=2 },
-                new Director { Name = "Christopher Nolan", MovieId=3 },
+                new Director { Name = "Kurt Wimmer"},
+                new Director { Name = "Christopher Nolan"},
                 new Director { Name = "Zekinha Snydero" }
 
             };
             directors.ForEach(director => context.Directors.Add(director));
             context.SaveChanges();
 
+
             var genres = new List<Genre>
             {
-                new Genre {MovieId = 1, GenreName=GenreName.Action},
-                new Genre {MovieId = 1, GenreName=GenreName.Drama},
-                new Genre {MovieId = 1, GenreName=GenreName.Sci_Fi},
-                new Genre {MovieId = 1, GenreName=GenreName.Thriller},
-                new Genre {MovieId = 2, GenreName=GenreName.Action},
-                new Genre {MovieId = 2, GenreName=GenreName.Adventure},
-                new Genre {MovieId = 2, GenreName=GenreName.Sci_Fi},
-                new Genre {MovieId = 2, GenreName=GenreName.Thriller},
-                new Genre {MovieId = 3, GenreName=GenreName.Adventure},
-                new Genre {MovieId = 3, GenreName=GenreName.Drama},
-                new Genre {MovieId = 3, GenreName=GenreName.Sci_Fi}
+                new Genre { GenreName= "Action"},
+                new Genre { GenreName= "Adventure"},
+                new Genre { GenreName= "Animation"},
+                new Genre { GenreName= "Biography"},
+                new Genre { GenreName= "Comedy"},
+                new Genre { GenreName= "Crime"},
+                new Genre { GenreName= "Documentary"},
+                new Genre { GenreName= "Drama"},
+                new Genre { GenreName= "Family"},
+                new Genre { GenreName= "Fantasy"},
+                new Genre { GenreName= "History"},
+                new Genre { GenreName= "Horror"},
+                new Genre { GenreName= "Music/Musical"},
+                new Genre { GenreName= "Mystery"},
+                new Genre { GenreName= "Romance"},
+                new Genre { GenreName= "Sci-Fi"},
+                new Genre { GenreName= "Sport"},
+                new Genre { GenreName= "Thriller"},
+                new Genre { GenreName= "War"},
+                new Genre { GenreName= "Western"}
             };
             genres.ForEach(genre => context.Genres.Add(genre));
             context.SaveChanges();
         }
-
     }
 }
