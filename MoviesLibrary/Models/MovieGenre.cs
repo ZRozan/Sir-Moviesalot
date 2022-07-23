38b231937fcd117e4625eed8c6a464dae4009e10
Jairo Rozan <jairo.rosa.z@hotmail.com>
2022-07-23 18:50:52 -0300

    Created navigation properties

---
+++ MoviesLibrary/Models/MovieGenre.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MoviesLibrary.Models
+{
+    public class MovieGenre
+    {
+        public int Id { get; set; }
+    }
+}
