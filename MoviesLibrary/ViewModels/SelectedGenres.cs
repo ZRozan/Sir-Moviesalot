9d50750df311cec14c3dc6f713fa8c4d595ee7ea
Jairo Rozan <jairo.rosa.z@hotmail.com>
2022-07-30 21:15:32 -0300

    Created ViewModel

---
+++ MoviesLibrary/ViewModels/SelectedGenres.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MoviesLibrary.ViewModels
+{
+    // Instructor = movies
+    public class SelectedGenres
+    {
+        public int GenresId { get; set; }
+        public string GenreName { get; set; }
+        public bool IsSelected { get; set; }
+    }
+}
