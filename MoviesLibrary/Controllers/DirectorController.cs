3e594d0e7e40327c52d5ec1e4160fc0c1a50ce14
Jairo Rozan <jairo.rosa.z@hotmail.com>
2022-07-30 22:17:20 -0300

    Edited Director views and controller

--- MoviesLibrary/Controllers/DirectorController.cs
+++ MoviesLibrary/Controllers/DirectorController.cs
@@ -47,7 +47,7 @@ namespace MoviesLibrary.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Name")] Director director)
+        public ActionResult Create([Bind(Include = "Id,Name,Biography,Picture")] Director director)
         {
             if (ModelState.IsValid)
             {
@@ -79,7 +79,7 @@ namespace MoviesLibrary.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Name")] Director director)
+        public ActionResult Edit([Bind(Include = "Id,Name,Biography,Picture")] Director director)
         {
             if (ModelState.IsValid)
             {
